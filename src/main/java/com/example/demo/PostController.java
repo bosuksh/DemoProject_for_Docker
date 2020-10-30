e8c9be4db5aaf3df7a0fbe2410736f9df52073f5
CORP\83210 <bosuklsh@lgcns.com>
2020-10-30 13:25:49 +0900

    [update] PostController post controller 수정

--- src/main/java/com/example/demo/PostController.java
+++ src/main/java/com/example/demo/PostController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import lombok.extern.log4j.Log4j;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
@@ -7,32 +9,33 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/post")
+@RestController
 public class PostController {
 
     @Autowired
     private PostService postService;
 
-    @GetMapping()
+    @GetMapping(value = "/post")
     @ResponseStatus(code = HttpStatus.OK)
     public List<Post> getPosts() {
         return postService.getPosts();
     }
 
     @CrossOrigin
-    @PostMapping()
+    @PostMapping(value = "/post")
     @ResponseStatus(code = HttpStatus.CREATED)
     public Post addPosts(@RequestBody Post request) {
+        System.out.println(request.getContent());
         return postService.addPost(request);
     }
 
-    @PutMapping()
+    @PutMapping(value = "/post")
     @ResponseStatus(code = HttpStatus.OK)
     public Post updatePosts(@RequestBody Post request) throws ChangeSetPersister.NotFoundException {
         return postService.updatePost(request);
     }
 
-    @DeleteMapping(path = "/{id}")
+    @DeleteMapping(path = "/post/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public Post deletePosts(@PathVariable Long id) throws ChangeSetPersister.NotFoundException {
         return postService.deletePost(id);
